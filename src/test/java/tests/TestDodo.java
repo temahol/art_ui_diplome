2048e3c6dc341af04489ba8c7eba323ec97a0dd0
Artem <aholevko@maxilect.com>
2023-04-19 12:01:20 +0300

    optimisation tests

--- src/test/java/tests/TestDodo.java
+++ src/test/java/tests/TestDodo.java
@@ -49,99 +49,6 @@ public class TestDodo extends TestBase {
 
     @Test
     @Tag("project")
-    public void choose3Pizza() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Open the main page of DoDo", () -> {
-            pages.openPage();
-        });
-        step("select correct City" + randomData.city, () -> {
-            elements.choozeCity(randomData.city);
-        });
-        step("check City" + randomData.city, () -> {
-            elements.checkCity(randomData.city);
-        });
-        step("select first pizza" + randomData.firstPizza, () -> {
-            elements.selectFood(randomData.firstPizza);
-        });
-        step("select second pizza" + randomData.secondPizza, () -> {
-            elements.selectFood(randomData.secondPizza);
-        });
-        step("select third pizza" + randomData.thirdPizza, () -> {
-            elements.selectFood(randomData.thirdPizza);
-        });
-        step("select Basket", () -> {
-            elements.selectBasket();
-        });
-        step("check first Pizza" + randomData.firstPizza, () -> {
-            elements.checkBasket(randomData.firstPizza);
-        });
-        step("check second Pizza" + randomData.secondPizza, () -> {
-            elements.checkBasket(randomData.secondPizza);
-        });
-        step("check third Pizza" + randomData.thirdPizza, () -> {
-            elements.checkBasket(randomData.thirdPizza);
-        });
-    }
-
-    @Test
-    @Tag("project")
-    public void chooseSnack() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Open the main page of DoDo", () -> {
-            pages.openPage();
-        });
-        step("select correct City" + randomData.city, () -> {
-            elements.choozeCity(randomData.city);
-        });
-        step("check City" + randomData.city, () -> {
-            elements.checkCity(randomData.city);
-        });
-        step("select item from header menu" + randomData.snacks, () -> {
-            elements.selectHeaderTab(randomData.snacks);
-        });
-        step("add shack to Basket" + randomData.snack, () -> {
-            elements.addToBasket(randomData.snack);
-        });
-        step("select Basket", () -> {
-            elements.selectBasket();
-        });
-        step("check snack" + randomData.snack, () -> {
-            elements.checkBasket(randomData.snack);
-        });
-    }
-
-    @Test
-    @Tag("project")
-    public void chooseCookie() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Open the main page of DoDo", () -> {
-            pages.openPage();
-        });
-        step("select correct City" + randomData.city, () -> {
-            elements.choozeCity(randomData.city);
-        });
-        step("check City" + randomData.city, () -> {
-            elements.checkCity(randomData.city);
-        });
-        step("select item from header menu" + randomData.cookies, () -> {
-            elements.selectHeaderTab(randomData.cookies);
-        });
-        step("add cookie to Basket" + randomData.cookie, () -> {
-            elements.addToBasket(randomData.cookie);
-        });
-        step("select Basket", () -> {
-            elements.selectBasket();
-        });
-        step("check cookie" + randomData.cookie, () -> {
-            elements.checkBasket(randomData.cookie);
-        });
-    }
-
-    @Test
-    @Tag("project")
     public void choosePizzaSize() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
@@ -178,28 +85,6 @@ public class TestDodo extends TestBase {
         });
         step("check About Us", () -> {
             elements.checkAboutUS();
-        });
-    }
-
-    @Test
-    @Tag("project")
-    public void dodoCoins() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Open the main page of DoDo", () -> {
-            pages.openPage();
-        });
-        step("select correct City" + randomData.city, () -> {
-            elements.choozeCity(randomData.city);
-        });
-        step("check City" + randomData.city, () -> {
-            elements.checkCity(randomData.city);
-        });
-        step("select item from header menu" + randomData.cookies, () -> {
-            elements.selectCoins();
-        });
-        step("check dodoCoins", () -> {
-            elements.checkCoins();
         });
     }
 }
